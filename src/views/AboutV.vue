299f82e5b740e9795354aa8c5f285a62bb23c361
Fra <fra24eso@gmail.com>
2024-05-19 02:14:40 +0200

    geolocationAPI: getCurrentPosition

--- src/views/AboutV.vue
+++ src/views/AboutV.vue
@@ -3,15 +3,14 @@
 </script>
 
 <template>
-  <div id="title" class="container mt-5">
-    <div class="row justify-content-center">
-      <div class="col-12 text-center">
-        <h1>Google Maps - API</h1>
-        <p>About🌐</p>
-      </div>
+    <div id="title" class="container mt-5">
+        <div class="row justify-content-center">
+            <div class="col-12 text-center">
+                <h1>Google Maps - API</h1>
+                <p>About🌐</p>
+            </div>
+        </div>
     </div>
-  </div>
 </template>
 
-<style>
-</style>
+<style></style>
--- src/views/HomeV.vue
+++ src/views/HomeV.vue
@@ -1,23 +1,42 @@
-<script setup>
-import { useRouter } from 'vue-router';
-
-const router = useRouter();
-const redirectToNews = () => {
-    router.push('/newscomponent');
+<script>
+export default {
+    methods: {
+        locatorButtonPressed() {
+            if (navigator.geolocation) {
+                navigator.geolocation.getCurrentPosition(
+                    (position) => {
+                        console.log(position.coords.latitude);
+                        console.log(position.coords.longitude);
+                    },
+                    (error) => {
+                        console.log(error.message);
+                    }
+                );
+            } else {
+                console.log("Error! Your browser does not support geolocation API");
+            }
+        },
+    },
 };
 </script>
 
 <template>
-  <div id="title" class="container mt-5">
-    <div class="row justify-content-center">
-      <div class="col-12 text-center">
-        <h1>Google Maps - API</h1>
-        <p>Home🏡</p>
-      </div>
+    <div id="title" class="container mt-5">
+        <div class="row justify-content-center">
+            <div class="col-12 col-lg-6 text-center">
+                <h1>Google Maps - API</h1>
+                <p>Home🏡</p>
+                <form action="">
+                    <div class="input-group flex-nowrap">
+                        <input type="text" class="form-control" placeholder="Enter your address" aria-label="Username"
+                            aria-describedby="addon-wrapping" />
+                        <button class="input-group-text bg-warning text-white"
+                            @click.prevent="locatorButtonPressed">🔎</button>
+                    </div>
+                </form>
+            </div>
+        </div>
     </div>
-  </div>
-  
 </template>
 
-<style>
-</style>
+<style></style>
